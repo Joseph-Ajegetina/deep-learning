bea0790f9e633c7a36c2986a7dcbbf0a5b583c7d
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-10-26 22:20:09 +0000

    new: clean libraspeech to become standalone push updated

--- librispeech-100/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-LibriSpeech Speech Recognition Package
-
-A complete implementation of vanilla RNN and RNN with Attention
-for Automatic Speech Recognition on the LibriSpeech corpus.
-"""
-
-__version__ = "1.0.0"
